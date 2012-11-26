2ed5a9dea1f1ed9e894c3d352872dc2ae8151ddb
Rod Mcnew <rmcnew@relivinc.com>
2012-11-26 08:44:43 -0600

    online app now reads settings form db

--- src/Rcm/Exception/PluginDataNotFoundException.php
+++ src/Rcm/Exception/PluginDataNotFoundException.php
@@ -17,7 +17,7 @@
  * @version   GIT: <git_id>
  * @link      http://ci.reliv.com/confluence
  */
-namespace RcmJsonDataPluginToolkit\Exception;
+namespace Rcm\Exception;
 
 /**
  * Reliv Common's Invalid Argument Exception
