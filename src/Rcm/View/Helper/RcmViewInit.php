a1f531cb7518824cecd180bd0e3edc6e6dd0b82d
Rod Mcnew <rmcnew@relivinc.com>
2013-01-24 16:01:35 -0600

    move prompt helper to make open sourcing and deprecation

--- src/Rcm/View/Helper/RcmViewInit.php
+++ src/Rcm/View/Helper/RcmViewInit.php
@@ -167,12 +167,12 @@ class RcmViewInit extends AbstractHelper
         );
 
         $renderer->headScript()->appendFile(
-            $renderer->basePath() . '/modules/rcm/vendor/alert-confirm/alert-confirm.js',
+            $renderer->basePath() . '/modules/rcm/vendor/jquery-ui-alert-confirm/jquery-ui-alert-confirm.js',
             'text/javascript'
         );
 
         $renderer->headScript()->appendFile(
-            $renderer->basePath() . '/modules/rcm/vendor/dialog-inputs/dialog-inputs.js',
+            $renderer->basePath() . '/modules/rcm/vendor/jquery-dialog-inputs/jquery-dialog-inputs.js',
             'text/javascript'
         );
 
