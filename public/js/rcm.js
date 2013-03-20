47879502970a3706b7ee7fc94eececfc42d0e8da
Westin Shafer <wshafer@relivinc.com>
2013-03-20 16:25:07 -0500

    Added redirect to login success callback

--- public/js/rcm.js
+++ public/js/rcm.js
@@ -15,7 +15,7 @@ function Rcm() {
 
     me.getPluginContainer = function(instanceId){
         return $(me.getPluginContainerSelector(instanceId));
-    }
+    };
 
     me.getPluginContainerSelector = function(instanceId){
 
@@ -27,7 +27,7 @@ function Rcm() {
         } else {
             return('#RcmRealPage [data-rcmPluginInstanceId="'+instanceId+'"] .rcmPluginContainer');
         }
-    }
+    };
 
 
     /**
@@ -53,5 +53,50 @@ function Rcm() {
             }
         }
         return params;
-    }
+    };
+
+    me.updateURLParameter = function(url, param, paramVal)
+    {
+        var TheAnchor = null;
+        var newAdditionalURL = "";
+        var tempArray = url.split("?");
+        var baseURL = tempArray[0];
+        var additionalURL = tempArray[1];
+        var temp = "";
+
+        if (additionalURL)
+        {
+            var tmpAnchor = additionalURL.split("#");
+            var TheParams = tmpAnchor[0];
+            TheAnchor = tmpAnchor[1];
+            if(TheAnchor)
+                additionalURL = TheParams;
+
+            tempArray = additionalURL.split("&");
+
+            for (i=0; i<tempArray.length; i++)
+            {
+                if(tempArray[i].split('=')[0] != param)
+                {
+                    newAdditionalURL += temp + tempArray[i];
+                    temp = "&";
+                }
+            }
+        }
+        else
+        {
+            var tmpAnchor = baseURL.split("#");
+            var TheParams = tmpAnchor[0];
+            TheAnchor  = tmpAnchor[1];
+
+            if(TheParams)
+                baseURL = TheParams;
+        }
+
+        if(TheAnchor)
+            paramVal += "#" + TheAnchor;
+
+        var rows_txt = temp + "" + param + "=" + paramVal;
+        return baseURL + "?" + newAdditionalURL + rows_txt;
+    };
 }
