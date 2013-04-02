e3f685668077ee58aa62dd40b2650e2813b667a6
Rod Mcnew <rmcnew@relivinc.com>
2013-04-02 15:15:00 -0500

    product option select - display

--- public/js/admin/prompt-helper.js
+++ public/js/admin/prompt-helper.js
@@ -300,19 +300,19 @@
         return this;
     };
 
-    /**
-     * DEPRECATED - DO NOT USE
-     * @return {String}
-     */
-    $.fn.generateUUID = function(){
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
-            /[xy]/g,
-            function(c) {
-                var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-                return v.toString(16);
-            }
-        );
-    };
+//    /**
+//     * DEPRECATED - DO NOT USE
+//     * @return {String}
+//     */
+//    $.fn.generateUUID = function(){
+//        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+//            /[xy]/g,
+//            function(c) {
+//                var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+//                return v.toString(16);
+//            }
+//        );
+//    };
 
     $.fn.validateInput = function(inputField, resultContainer, ajaxPath, disallowSpaces) {
 
