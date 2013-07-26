b4b43d24ba7b8142d21bc26ea1d17b31bae384c6
Westin Shafer <wshafer@relivinc.com>
2013-07-26 15:20:24 -0500

    Change Product Edit to use Post instead of Get

--- public/js/admin/content-manager2.js
+++ public/js/admin/content-manager2.js
@@ -1845,38 +1845,45 @@ function RcmEdit(config) {
     me.saveAjaxAdminWindow = function (saveUrl, send, formContainer, dataOkHeadline, dataOkMessage, keepOpen, successCallback) {
         $.getJSON(saveUrl,
             send,
-            function (data) {
-                if (data.dataOk == 'Y' && data.redirect == undefined) {
-                    //Close Window unless told not to
-                    if (keepOpen !== true) {
-                        $(formContainer).parent().dialog("close");
-                    } else {
-                        $(formContainer).find(".ajaxFormErrorLine").html('').hide();
-                    }
-
-                    //Show Status Message if passed in
-                    if (dataOkHeadline && dataOkMessage) {
-                        $.growlUI(dataOkHeadline, dataOkMessage);
-                    }
-
-                    //Process sucessCallback if passed in
-                    if (typeof successCallback === 'function') {
-                        successCallback(data);
-                    }
-                } else if (data.dataOk == 'Y' && data.redirect) {
-                    window.location = data.redirect;
-                } else if (data.dataOk != 'Y' && data.error != '') {
-                    $(formContainer).find(".ajaxFormErrorLine").html('<br /><p style="color: #FF0000;">' + data.error + '</p><br />').show();
-                    $(formContainer).parent().scrollTop(0);
-                } else {
-                    $(formContainer).find(".ajaxFormErrorLine").html('<br /><p style="color: #FF0000;">Communication Error!</p><br />').show();
-                    $(formContainer).parent().scrollTop(0);
-                }
-            }
+            function(data) {me.saveAjaxAdminWindowSuccess(data, formContainer, dataOkHeadline, dataOkMessage, keepOpen, successCallback) }
         ).error(function () {
-                $(formContainer).find(".ajaxFormErrorLine").html('<br /><p style="color: #FF0000;">Communication Error!</p><br />').show();
-                $(formContainer).parent().scrollTop(0);
-            })
+                me.saveAjaxAdminWindowSuccessError(formContainer);
+        });
+    };
+
+    me.saveAjaxAdminWindowSuccess = function(data, formContainer, dataOkHeadline, dataOkMessage, keepOpen, successCallback) {
+        if (data.dataOk == 'Y' && data.redirect == undefined) {
+            //Close Window unless told not to
+            if (keepOpen !== true) {
+                $(formContainer).parent().dialog("close");
+            } else {
+                $(formContainer).find(".ajaxFormErrorLine").html('').hide();
+            }
+
+            //Show Status Message if passed in
+            if (dataOkHeadline && dataOkMessage) {
+                $.growlUI(dataOkHeadline, dataOkMessage);
+            }
+
+            //Process sucessCallback if passed in
+            if (typeof successCallback === 'function') {
+                successCallback(data);
+            }
+        } else if (data.dataOk == 'Y' && data.redirect) {
+            window.location = data.redirect;
+        } else if (data.dataOk != 'Y' && data.error != '') {
+            $(formContainer).find(".ajaxFormErrorLine").html('<br /><p style="color: #FF0000;">' + data.error + '</p><br />').show();
+            $(formContainer).parent().scrollTop(0);
+        } else {
+            $(formContainer).find(".ajaxFormErrorLine").html('<br /><p style="color: #FF0000;">Communication Error!</p><br />').show();
+            $(formContainer).parent().scrollTop(0);
+        }
+
+    };
+
+    me.saveAjaxAdminWindowSuccessError = function(formContainer) {
+        $(formContainer).find(".ajaxFormErrorLine").html('<br /><p style="color: #FF0000;">Communication Error!</p><br />').show();
+        $(formContainer).parent().scrollTop(0);
     };
 
     /**
