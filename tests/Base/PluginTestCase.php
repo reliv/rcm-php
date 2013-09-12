99bd0aececd7876b2748f097ea2b486e87695c5a
Brian Janish <bjanish@relivinc.com>
2013-09-12 10:12:41 -0500

    Fix for VIP code call. / Added Unit test for profile script

---
+++ tests/Base/PluginTestCase.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Rcm\Tests\Base;
+
+use Rcm\Entity\Country;
+use Rcm\Entity\Language;
+
+class PluginTestCase extends DoctrineTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+
+    }
+
+    public function createDefaultSiteInstance()
+    {
+        $country = $this->getDefaultCountry();
+        $language = $this->getDefaultLanguage();
+
+
+    }
+
+    public function getDefaultCountry()
+    {
+        $country = new Country();
+        $country->setCountryName('United States');
+        $country->setIso2('US');
+        $country->setIso3('USA');
+        $this->entityManager->persist($country);
+        $this->entityManager->flush();
+
+        return $country;
+    }
+
+    public function getDefaultLanguage()
+    {
+        $language = new Language();
+        $language->setLanguageName('English');
+        $language->setIso6391('en');
+        $language->setIso6392b('eng');
+        $language->setIso6392t('eng');
+
+        $this->entityManager->persist($language);
+        $this->entityManager->flush();
+
+        return $language;
+    }
+}
