9923b5e5196f8131c7b6e69fbdd51a7c77b3dcab
Rod Mcnew <rmcnew@relivinc.com>
2013-12-09 14:27:51 -0600

    remove dup funcs

--- test/Base/ZF2TestCase.php
+++ test/Base/ZF2TestCase.php
@@ -190,32 +190,4 @@ class Zf2TestCase extends \PHPUnit_Framework_TestCase
 
         return $this->renderer;
     }
-
-    public function getSiteMock()
-    {
-        $site = new Site();
-        $site->setLanguage($this->getLanguageMock());
-        $site->setCountry($this->getCountryMock());
-    }
-
-    public function getCountryMock()
-    {
-        $country = new Country();
-        $country->setCountryName('United States');
-        $country->setIso2('US');
-        $country->setIso3('USA');
-
-        return $country;
-    }
-
-    public function getLanguageMock()
-    {
-        $language = new Language();
-        $language->setLanguageName('English');
-        $language->setIso6391('en');
-        $language->setIso6392b('eng');
-        $language->setIso6392t('eng');
-
-        return $language;
-    }
 }
