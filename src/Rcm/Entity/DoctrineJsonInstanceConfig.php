03ca849b412caaf818e01ff071a7ac4af33e77b8
Rod McNew <rmcnew@relivinc.com>
2014-07-22 10:17:05 -0500

    fix checkstyle error & remove obsolute username validator (there is a newer better one and htis one is not used)

--- src/Rcm/Entity/DoctrineJsonInstanceConfig.php
+++ src/Rcm/Entity/DoctrineJsonInstanceConfig.php
@@ -96,7 +96,7 @@ class DoctrineJsonInstanceConfig
     /**
      * Sets the config that will be stored in the DB as JSON
      *
-     * @param array $config new value
+     * @param array $config new value test
      *
      * @return null
      *
