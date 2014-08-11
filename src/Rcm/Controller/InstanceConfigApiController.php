dfa8d9128bde5a531bf0fc08703e1480fecd7422
Rod McNew <rmcnew@relivinc.com>
2014-08-11 10:43:38 -0500

    rebiuld admin instance config api

---
+++ src/Rcm/Controller/InstanceConfigApiController.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * InstanceConfigApiController.php
+ *
+ * PHP version 5
+ *
+ * @category  Reliv
+ * @package   Rcm\Controller\Plugin
+ * @author    Rod Mcnew <rmcnew@relivinc.com>
+ * @copyright 2014 Reliv International
+ * @license   License.txt New BSD License
+ * @version   GIT: <git_id>
+ * @link      https://github.com/reliv
+ */
+
+namespace Rcm\Controller;
+
+use Rcm\Exception\PluginInstanceNotFoundException;
+use Zend\Mvc\Controller\AbstractRestfulController;
+use Zend\View\Model\JsonModel;
+
+
+/**
+ * InstanceConfigApiController
+ *
+ * PHP version 5
+ *
+ * @category  Reliv
+ * @package   Rcm\Controller\Plugin
+ * @author    Rod Mcnew <rmcnew@relivinc.com>
+ * @copyright 2014 Reliv International
+ * @license   License.txt New BSD License
+ * @version   Release: <package_version>
+ * @link      https://github.com/reliv
+ */
+class InstanceConfigApiController extends AbstractRestfulController
+{
+    public function get($id)
+    {
+        $siteId = $this->getServiceLocator()->get('Rcm\Service\SiteManager')
+            ->getCurrentSiteId();
+        $allowed = $this->getServiceLocator()->get('RcmUser\Service\RcmUserService')->isAllowed(
+            'sites.' . $siteId,
+            'admin',
+            'Rcm\Acl\ResourceProvider'
+        );
+
+        if (!$allowed) {
+            $this->getResponse()->setStatusCode(401);
+            return $this->getResponse();
+        }
+        try {
+            $instanceConfig = $this->getServiceLocator()
+                ->get('Rcm\Service\PluginManager')->getInstanceConfig($id);
+        } catch (PluginInstanceNotFoundException $e) {
+            return $this->notFoundAction();
+        }
+        return new JsonModel($instanceConfig);
+    }
+}
