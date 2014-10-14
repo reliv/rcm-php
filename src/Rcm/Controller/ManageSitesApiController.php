578a9269e8b78ddd3d056bc15dc621b55f533cde
bjanish <brian@janish.org>
2014-10-14 10:57:19 -0500

    Disable sites Enable sites (not finished yet)

--- src/Rcm/Controller/ManageSitesApiController.php
+++ src/Rcm/Controller/ManageSitesApiController.php
@@ -15,6 +15,7 @@
 
 namespace Rcm\Controller;
 
+use Rcm\Http\Response;
 use Zend\Mvc\Controller\AbstractRestfulController;
 use Zend\View\Model\JsonModel;
 
@@ -36,25 +37,95 @@ class ManageSitesApiController extends AbstractRestfulController
 {
     public function getList()
     {
+
         /**
          * @var $siteManager \Rcm\Service\SiteManager
          */
         $siteManager = $this->getServiceLocator()->get(
             'Rcm\Service\SiteManager'
         );
+
         $sitesObjects = $siteManager->getAllSites();
+
         $sites = [];
+
         foreach ($sitesObjects as $site) {
             $domain = null; //'[no domains found for this site]';
+
             if (is_object($site->getDomain())) {
                 $domain = $site->getDomain()->getDomainName();
             }
+//            if ($site === reset($site))
+//                echo 'FIRST ELEMENT!'.$site;
+
+//            $temp = $site->getStatus();
+//            echo $temp;
             $sites[] = [
                 'siteId' => $site->getSiteId(),
                 'domain' => $domain,
-                'active' => $site->getStatus() == 'A'
+                'active' => $site->getStatus(),
             ];
         }
         return new JsonModel($sites);
     }
-} 
+
+    /**
+     * update
+     *
+     * @param mixed $siteId
+     * @param mixed $data
+     *
+     * @return mixed|JsonModel
+     */
+    public function update($siteId, $data)
+    {
+//        var_dump($data);
+        // Check if siteId is valid
+        // Store value to make site disabled
+
+
+        //CREATE RESOURCE ID
+
+        //ACCESS CHECK
+//        if (!$this->rcmUserIsAllowed('sites', 'admin', 'RcmAdmin')) {
+//            $this->getResponse()->setStatusCode(Response::STATUS_CODE_401);
+//            return $this->getResponse();
+//        }
+        /**
+         * @var $siteManager \Rcm\Service\SiteManager
+         */
+        $siteManager = $this->getServiceLocator()->get(
+            'Rcm\Service\SiteManager'
+        );
+
+        if (!$siteManager->isValidSiteId($siteId)) {
+            $this->getResponse()->setStatusCode(Response::STATUS_CODE_400);
+            return $this->getResponse();
+        }
+
+        $routeMatch = $this->getEvent()->getRouteMatch();
+        $status = $routeMatch->getParam('active');
+
+        echo 'status = '.$status;
+        $site = $siteManager->getSiteById($siteId);
+
+        if ($status == 'D') {
+            $site->setStatus('D');
+        }
+        if ($status == 'A')
+            $site->setStatus('A');
+
+        $em = $siteManager->getSiteRepo()->getDoctrine();
+
+        $em->persist($site);
+        $em->flush();
+
+        return new JsonModel(
+            array(
+                $site->getSiteId(),
+                $site->getStatus()
+            )
+        );
+    }
+
+}
