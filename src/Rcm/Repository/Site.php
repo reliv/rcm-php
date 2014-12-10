f32b89b051c163515bf6cdf35042076aedb34422
Westin Shafer <wshafer@relivinc.com>
2014-12-10 14:33:25 -0600

    Hotfix for domain issue

--- src/Rcm/Repository/Site.php
+++ src/Rcm/Repository/Site.php
@@ -205,6 +205,7 @@ class Site extends EntityRepository
         if ($domain->getPrimary()) {
             $site = $domain->getPrimary()->getSite();
             $this->_em->detach($site);
+            $this->_em->detach($domain);
             $site->setDomain($domain);
         } else {
             $site = $domain->getSite();
