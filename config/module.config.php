fdf3e39fb2ff45c63622e4c1c6e0bcba8182e3fa
Rod McNew <rmcnew@relivinc.com>
2015-03-13 14:48:10 -0500

    make load balance health check route

--- config/module.config.php
+++ config/module.config.php
@@ -181,10 +181,10 @@ return [
                     ]
                 ],
             ],
-            'load-balancer/health-check' => [
+            'load-balancer-health-check' => [
                 'type' => 'Zend\Mvc\Router\Http\Segment',
                 'options' => [
-                    'route' => '/load-balancer/health-check',
+                    'route' => '/load-balancer-health-check',
                     'defaults' => [
                         'controller' => 'Rcm\Controller\PageSearchApiController',
                         'action' => 'allSitePages',
