aa83d1c6527b50937a1d0a676f169a8b356a42e3
Westin Shafer <wshafer@relivinc.com>
2015-04-24 17:33:42 -0500

    Move to PSR 4

--- src/Module.php
+++ src/Module.php
@@ -102,27 +102,6 @@ class Module
     }
 
     /**
-     * getAutoloaderConfig() is a requirement for all Modules in ZF2.  This
-     * function is included as part of that standard.  See Docs on ZF2 for more
-     * information.
-     *
-     * @return array Returns array to be used by the ZF2 Module Manager
-     */
-    public function getAutoloaderConfig()
-    {
-        return [
-            'Zend\Loader\ClassMapAutoloader' => [
-                __DIR__ . '/autoload_classmap.php',
-            ],
-            'Zend\Loader\StandardAutoloader' => [
-                'namespaces' => [
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ],
-            ],
-        ];
-    }
-
-    /**
      * getConfig() is a requirement for all Modules in ZF2.  This
      * function is included as part of that standard.  See Docs on ZF2 for more
      * information.
@@ -131,6 +110,6 @@ class Module
      */
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../config/module.config.php';
     }
 }
