5bacadb5302ddb42787d7bfa39328ee2cfc05aad
James Jervis <jjervis@relivinc.com>
2015-07-08 09:42:51 -0500

    Fix js issue

--- public/rcm-page-admin/angular-rcm-admin.js
+++ public/rcm-page-admin/angular-rcm-admin.js
@@ -8,7 +8,7 @@
  */
 angular.module(
     'rcmAdmin',
-    ['rcmApi', 'rcmAdminApi', 'RcmHtmlEditor']
+    ['rcmApi', 'rcmAdminApi', 'RcmHtmlEditor', 'angularUtils.directives.dirPagination']
 )
 /**
  * rcmAdminService
