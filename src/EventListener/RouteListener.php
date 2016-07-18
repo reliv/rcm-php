2cf36fe9311872eb5e0b288b2875311cb5c1f324
rodmcnew <rodmcnew@gmail.com>
2016-07-18 16:49:03 -0500

    optimize redirect speed

--- src/EventListener/RouteListener.php
+++ src/EventListener/RouteListener.php
@@ -138,7 +138,13 @@ class RouteListener
         $baseUri = explode('?', $requestUri);
         $requestUrl = $baseUri[0];
 
+        $startTime = microtime(true);
+
+
         $redirect = $this->redirectRepo->getRedirect($requestUrl, $siteId);
+
+        var_dump(round((microtime(true) - $startTime) * 1000,2) . 'ms');
+        exit;
 
         if (!empty($redirect)) {
             header('Location: ' . $redirect->getRedirectUrl(), true, 302);
