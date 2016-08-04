6b6f6bb90226f488346398c8cb7db2f048a5db01
James Jervis <jjervis@relivinc.com>
2016-08-04 14:23:20 -0500

    Add place-holders so assets do not break

--- config/asset_manager.php
+++ config/asset_manager.php
@@ -6,6 +6,7 @@ $bowerComponentsDir = __DIR__ . '/../../../../public/bower_components/';
 return [
     'resolver_configs' => [
         'aliases' => [
+            'modules/rcm/' => __DIR__ . '/../public/',
             'bower_components/' => $bowerComponentsDir,
             // Legacy support because bower_components used to be named "vendor"
             'vendor/' => $bowerComponentsDir
@@ -15,14 +16,22 @@ return [
              * Core JS and css
              * (core features)
              */
-            'modules/rcm/rcm.js' => [],
-            'modules/rcm/rcm.css' => [],
+            'modules/rcm/rcm.js' => [
+                'modules/rcm/place-holder.js'
+            ],
+            'modules/rcm/rcm.css' => [
+                'modules/rcm/place-holder.css'
+            ],
             /**
              * Extended JS and css
              * (features for modules and lower level services)
              */
-            'modules/rcm/modules.js' => [],
-            'modules/rcm/modules.css' => [],
+            'modules/rcm/modules.js' => [
+                'modules/rcm/place-holder.js'
+            ],
+            'modules/rcm/modules.css' => [
+                'modules/rcm/place-holder.css'
+            ],
         ],
     ],
 ];
