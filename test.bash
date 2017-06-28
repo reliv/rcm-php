1df164a9f994eabff67e1a72c68ecb31fd9c5597
James Jervis <jjervis@relivinc.com>
2017-06-28 10:23:45 -0500

    Update composer and travis

---
+++ test.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo ""
+echo "[test.bash] Running Composer install..."
+composer self-update
+composer --version
+composer global require fxp/composer-asset-plugin --no-plugins
+composer install --prefer-dist
+
+echo ""
+echo "[test.bash] Running PHP unit tests..."
+php ./vendor/bin/phpunit
+
+echo ""
+echo "[test.bash] Running PHP Code Sniffer..."
+php ./vendor/bin/phpcs --standard=PSR2 --ignore=vendor,test,config,data,autoload_classmap.php --extensions=php ./
