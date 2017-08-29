5ba2e43b207804988ae8233ba7b6f0736464544b
James Jervis <jjervis@relivinc.com>
2017-08-29 14:23:58 -0500

    Add deprecation

--- src/Service/CurrentSite.php
+++ src/Service/CurrentSite.php
@@ -5,6 +5,8 @@ namespace Rcm\Service;
 use Rcm\Entity\Site;
 
 /**
+ * @deprecated This service is stateful per request
+ *
  * Place holder for service
  * @author James Jervis - https://github.com/jerv13
  */
