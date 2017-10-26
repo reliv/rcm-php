8f7bdd18996e48070975010257029b9f2d6ffcdc
rodmcnew <rodmcnew@gmail.com>
2017-10-26 11:23:51 -0500

    update copywrite dates

--- src/Exception/CountryNotFoundException.php
+++ src/Exception/CountryNotFoundException.php
@@ -12,7 +12,7 @@ use Rcm\Exception\ExceptionInterface as RcmExceptionInterface;
  * @category  Reliv
  * @package   Rcm\Exception
  * @author    James Jervis <jjervis@relivinc.com>
- * @copyright 2014 Reliv International
+ * @copyright 2017 Reliv International
  * @license   License.txt New BSD License
  * @version   Release: <package_version>
  * @link      https://github.com/reliv
--- src/Factory/RcmIsPageAllowedPluginFactory.php
+++ src/Factory/RcmIsPageAllowedPluginFactory.php
@@ -15,7 +15,7 @@ use Zend\ServiceManager\ServiceLocatorInterface;
  * @category  Reliv
  * @package   Rcm
  * @author    Westin Shafer <wshafer@relivinc.com>
- * @copyright 2014 Reliv International
+ * @copyright 2017 Reliv International
  * @license   License.txt New BSD License
  * @version   GIT: <git_id>
  * @link      https://github.com/reliv
