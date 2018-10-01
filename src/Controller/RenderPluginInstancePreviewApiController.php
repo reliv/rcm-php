86e3d3ce91adc9cfe03f45be68962a56feba2791
Rod McNew <rmcnew@relivinc.com>
2018-10-01 18:11:01 -0500

    axo#22574 fix CMS block instance config change previewer (clean code)

--- src/Controller/RenderPluginInstancePreviewApiController.php
+++ src/Controller/RenderPluginInstancePreviewApiController.php
@@ -30,8 +30,7 @@ class RenderPluginInstancePreviewApiController extends AbstractActionController
             true
         );
 
-        if (
-            json_last_error() !== JSON_ERROR_NONE
+        if (json_last_error() !== JSON_ERROR_NONE
             || !is_array($data)
             || !array_key_exists('pluginType', $data)
             || !array_key_exists('instanceId', $data)
