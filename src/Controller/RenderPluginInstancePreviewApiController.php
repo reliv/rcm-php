32bd013361dc8300ea59fa8033c5717b1e82d168
Rod McNew <rmcnew@relivinc.com>
2018-10-02 09:49:18 -0500

    axo#22574 fix CMS block instance config change previewer (improve error message)

--- src/Controller/RenderPluginInstancePreviewApiController.php
+++ src/Controller/RenderPluginInstancePreviewApiController.php
@@ -37,7 +37,10 @@ class RenderPluginInstancePreviewApiController extends AbstractActionController
             || !array_key_exists('instanceConfig', $data)
         ) {
             $response = new Response();
-            $response->setContent('400 Bad Request');
+            $response->setContent(
+                '400 Bad Request - Request must be a JSON object'
+                . ' that contains properties pluginType, instanceId, and instanceConfig'
+            );
             $response->setStatusCode(400);
 
             return $response;
