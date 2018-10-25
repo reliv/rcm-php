7ab097419cac6fa30567657a36f57d8470ab7999
Rod McNew <rmcnew@relivinc.com>
2018-10-25 15:40:12 -0500

    work on proof of concept of immutable history

--- immutable-history/src/ModuleConfig.php
+++ immutable-history/src/ModuleConfig.php
@@ -2,10 +2,42 @@
 
 namespace Rcm\ImmutableHistory;
 
+use Rcm\ImmutableHistory\Entity\ImmutablePageVersion;
+
 class ModuleConfig
 {
     public function __invoke()
     {
-        return [];
+        return [
+            'depedencies' => [
+                [
+                    'config_factories' => [
+                        'Rcm\ImmutableHistory\PageVersionRepo' => [
+                            'class' => VersionRepository::class,
+                            'arguments' => [
+                                ['literal' => ImmutablePageVersion::class],
+                                Doctrine\ORM\EntityManager::class
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            'doctrine' => [
+                'driver' => [
+                    'Rcm\ImmutableHistory' => [
+                        'class' => \Doctrine\ORM\Mapping\Driver\AnnotationDriver::class,
+                        'cache' => 'array',
+                        'paths' => [
+                            __DIR__ . '/Entity'
+                        ]
+                    ],
+                    'orm_default' => [
+                        'drivers' => [
+                            'Rcm\ImmutableHistory' => 'Rcm\ImmutableHistory'
+                        ]
+                    ]
+                ],
+            ]
+        ];
     }
 }
