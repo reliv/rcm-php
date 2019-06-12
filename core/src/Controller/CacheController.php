5250a3f5f6934a0594021c117eed1ceadcc354f3
Rod McNew <rmcnew@relivinc.com>
2019-06-12 09:46:40 -0500

    Comment out "CacheController" since it doesn't work on load-balanced systems.

--- core/src/Controller/CacheController.php
+++ core/src/Controller/CacheController.php
@@ -1,33 +1,33 @@
 <?php
-
-namespace Rcm\Controller;
-
-use Zend\View\Model\ViewModel;
-
-/**
- * Cache Controller for the application
- *
- * Currently this controller just allows a person to flush the entire cache
- *
- * @category  Reliv
- * @package   Rcm
- * @author    Westin Shafer <wshafer@relivinc.com>
- * @copyright 2012 Reliv International
- * @license   License.txt New BSD License
- * @version   Release: 1.0
- * @link      http://github.com/reliv
- */
-class CacheController extends AbstractActionController
-{
-    /**
-     * Index Action.  Main action for page in the CMS.
-     *
-     * @return ViewModel
-     */
-    public function flushAction()
-    {
-        /** @var \Zend\Cache\Storage\Adapter\Memory $cache */
-        $cache = $this->serviceLocator->get(\Rcm\Service\Cache::class);
-        $cache->flush();
-    }
-}
+//
+//namespace Rcm\Controller;
+//
+//use Zend\View\Model\ViewModel;
+//
+///**
+// * Cache Controller for the application
+// *
+// * Currently this controller just allows a person to flush the entire cache
+// *
+// * @category  Reliv
+// * @package   Rcm
+// * @author    Westin Shafer <wshafer@relivinc.com>
+// * @copyright 2012 Reliv International
+// * @license   License.txt New BSD License
+// * @version   Release: 1.0
+// * @link      http://github.com/reliv
+// */
+//class CacheController extends AbstractActionController
+//{
+//    /**
+//     * Index Action.  Main action for page in the CMS.
+//     *
+//     * @return ViewModel
+//     */
+//    public function flushAction()
+//    {
+//        /** @var \Zend\Cache\Storage\Adapter\Memory $cache */
+//        $cache = $this->serviceLocator->get(\Rcm\Service\Cache::class);
+//        $cache->flush();
+//    }
+//}
