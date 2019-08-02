6f5645e788ec4aa29857cff79072f94a215f1311
Rod McNew <rmcnew@relivinc.com>
2019-08-02 17:11:45 -0500

    disable current request module

--- current-request-disabled/src/CurrentRequest.php
+++ current-request-disabled/src/CurrentRequest.php
@@ -1,20 +1,20 @@
 <?php
-
-namespace Rcm\CurrentRequest;
-
-use Psr\Http\Message\ServerRequestInterface;
-use Zend\Diactoros\ServerRequest;
-
-/**
- * @deprecated user "current request context" instead.
- *
- * This is place-holder to allow the Rcm\CurrentRequest\CurrentRequest
- * service to auto-complete.
- *
- * Class CurrentRequest
- * @package Rcm\CurrentRequest
- */
-class CurrentRequest extends ServerRequest implements ServerRequestInterface
-{
-
-}
+//
+//namespace Rcm\CurrentRequest;
+//
+//use Psr\Http\Message\ServerRequestInterface;
+//use Zend\Diactoros\ServerRequest;
+//
+///**
+// * @deprecated user "current request context" instead.
+// *
+// * This is place-holder to allow the Rcm\CurrentRequest\CurrentRequest
+// * service to auto-complete.
+// *
+// * Class CurrentRequest
+// * @package Rcm\CurrentRequest
+// */
+//class CurrentRequest extends ServerRequest implements ServerRequestInterface
+//{
+//
+//}
