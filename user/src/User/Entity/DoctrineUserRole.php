456fa9cb6fdc7a64d3f2df31b0e59d0c253b4641
Rod McNew <rmcnew@relivinc.com>
2019-11-13 11:01:16 -0600

    Disable Old ACL table.

--- user/src/User/Entity/DoctrineUserRole.php
+++ user/src/User/Entity/DoctrineUserRole.php
@@ -1,32 +1,32 @@
 <?php
-
-namespace RcmUser\User\Entity;
-
-use Doctrine\ORM\Mapping as ORM;
-
-/**
- * @author James Jervis - https://github.com/jerv13
- *
- * @ORM\Entity
- * @ORM\Table(name="rcm_user_user_role")
- */
-class DoctrineUserRole extends UserRole
-{
-    /**
-     * @var integer $id
-     * @ORM\Id
-     * @ORM\Column(type="integer", nullable=false)
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    protected $id;
-    /**
-     * @var string $userId
-     * @ORM\Column(type="string", length=255, nullable=false)
-     */
-    protected $userId;
-    /**
-     * @var string $roleId
-     * @ORM\Column(type="string", length=255, nullable=false)
-     */
-    protected $roleId;
-}
+//
+//namespace RcmUser\User\Entity;
+//
+//use Doctrine\ORM\Mapping as ORM;
+//
+///**
+// * @author James Jervis - https://github.com/jerv13
+// *
+// * @ORM\Entity
+// * @ORM\Table(name="rcm_user_user_role")
+// */
+//class DoctrineUserRole extends UserRole
+//{
+//    /**
+//     * @var integer $id
+//     * @ORM\Id
+//     * @ORM\Column(type="integer", nullable=false)
+//     * @ORM\GeneratedValue(strategy="AUTO")
+//     */
+//    protected $id;
+//    /**
+//     * @var string $userId
+//     * @ORM\Column(type="string", length=255, nullable=false)
+//     */
+//    protected $userId;
+//    /**
+//     * @var string $roleId
+//     * @ORM\Column(type="string", length=255, nullable=false)
+//     */
+//    protected $roleId;
+//}
