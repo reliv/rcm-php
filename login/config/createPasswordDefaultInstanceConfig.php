4870b3c807880630b95ddef982904ccc5de6f06b
Rod McNew <rmcnew@relivinc.com>
2020-02-12 11:45:37 -0600

    25308	Fix many translation and style issues with reset-password system.

--- login/config/createPasswordDefaultInstanceConfig.php
+++ login/config/createPasswordDefaultInstanceConfig.php
@@ -1,19 +1,2 @@
 <?php
-return [
-//    'translate' => [
-//        'buttonProcessing' => 'Processing...',
-//        'passwordsDoNotMatch' => 'Passwords do not match',
-//        'invalidLink' => 'Link has expired or is invalid',
-//        'systemError' => 'A system error occurred'
-//    ],
-//    'thankYou' => '
-//        <br><p><strong>Your password has been changed.</strong></p>
-//    ',
-//    'top' => '
-//        <h1>Reset Password</h1>
-//    ',
-//    'passwordLabel' => 'New Password:',
-//    'passwordTwoLabel' => 'Re-enter new password:',
-//    'alphanumeric' => '(letters and numbers only)',
-//    'button' => 'Submit',
-];
+return [];
