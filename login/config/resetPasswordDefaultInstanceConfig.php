2326ba863725bb3e0a148c59ef785bc293794c99
Rod McNew <rmcnew@relivinc.com>
2020-02-12 11:43:34 -0600

    25308	Fix many translation and style issues with reset-password system.

--- login/config/resetPasswordDefaultInstanceConfig.php
+++ login/config/resetPasswordDefaultInstanceConfig.php
@@ -1,42 +1,42 @@
 <?php
-$hostname = 'localhost';
-
-if (!empty($_SERVER['HTTP_HOST'])) {
-    $hostname = $_SERVER['HTTP_HOST'];
-}
+//$hostname = 'localhost';
+//
+//if (!empty($_SERVER['HTTP_HOST'])) {
+//    $hostname = $_SERVER['HTTP_HOST'];
+//}
 
 return [
-    'top' => '
-        <h1>Reset Password</h1>
-        <div>
-        Enter your user name.  An email to reset your password will be sent to your email address on file.
-        </div>
-    ',
-    'passwordLabel' => 'New Password:',
-    'passwordTwoLabel' => 'Re-enter new password:',
-    'userIdLabel' => 'User Name:',
-    'button' => 'Submit',
-    'prospectEmail' => [
-        'fromEmail' => 'no-reply@'.$hostname,
-        'fromName' => $hostname.' Password Reset',
-        'subject' => 'Reset Password',
-        'body' => '<html>
-<body marginheight="0">
-    <table cellpadding="0" cellspacing="0" height="300" width="600" border="0">
-      <tbody>
-        <tr>
-          <td valign="top"><blockquote><font face="Verdana, Arial, Helvetica, sans-serif"
-              size="2"><br>
-              Click on the link below to reset your password:<br><br>
-             <strong>RCN</strong>: {userId}<br><br>
-             <a href="{url}">Reset your password.</a><br>
-
-            </font></blockquote></td>
-        </tr>
-      </tbody>
-    </table>
-</body>
-</html>
-'
-    ]
+//    'top' => '
+//        <h1>Reset Password</h1>
+//        <div>
+//        Enter your user name.  An email to reset your password will be sent to your email address on file.
+//        </div>
+//    ',
+//    'passwordLabel' => 'New Password:',
+//    'passwordTwoLabel' => 'Re-enter new password:',
+//    'userIdLabel' => 'User Name:',
+//    'button' => 'Submit',
+//    'prospectEmail' => [
+//        'fromEmail' => 'no-reply@'.$hostname,
+//        'fromName' => $hostname.' Password Reset',
+//        'subject' => 'Reset Password',
+//        'body' => '<html>
+//<body marginheight="0">
+//    <table cellpadding="0" cellspacing="0" height="300" width="600" border="0">
+//      <tbody>
+//        <tr>
+//          <td valign="top"><blockquote><font face="Verdana, Arial, Helvetica, sans-serif"
+//              size="2"><br>
+//              Click on the link below to reset your password:<br><br>
+//             <strong>RCN</strong>: {userId}<br><br>
+//             <a href="{url}">Reset your password.</a><br>
+//
+//            </font></blockquote></td>
+//        </tr>
+//      </tbody>
+//    </table>
+//</body>
+//</html>
+//'
+//    ]
 ];
--- login/src/Email/DefaultMailer.php
+++ login/src/Email/DefaultMailer.php
@@ -4,26 +4,21 @@ namespace RcmLogin\Email;
 
 use RcmLogin\Entity\ResetPassword;
 use RcmUser\User\Entity\UserInterface;
+use Reliv\App\Mailer\Service\TemplateMailer;
 use Zend\Mail\Exception\InvalidArgumentException;
 use Zend\Mail\Message;
 use Zend\Mime\Message as MimeMessage;
 use Zend\Mime\Part as MimePart;
 
-/**
- * Class Mailer
- *
- * PHP version 5
- *
- * @category  Reliv
- * @package   Mailer
- * @author    James Jervis <jjervis@relivinc.com>
- * @copyright 2016 Reliv International
- * @license   License.txt
- * @version   Release: <package_version>
- * @link      https://github.com/reliv
- */
 class DefaultMailer implements Mailer
 {
+    protected $templateMailer;
+
+    public function __construct(TemplateMailer $templateMailer)
+    {
+        $this->templateMailer = $templateMailer;
+    }
+
     /**
      * send
      *
@@ -49,51 +44,20 @@ class DefaultMailer implements Mailer
             return;
         }
 
-        $vars = [
-            'name' => '',
-            'userId' => $user->getId(),
-            'url' =>
-                'https://' . $_SERVER['HTTP_HOST']
-                . '/reset-password?fromPasswordResetEmail=1&id='
-                . $resetPassword->getResetId() . '&key='
-                . $resetPassword->getHashKey()
-        ];
+        $resetPasswordLinkUrl =
+            'https://' . $_SERVER['HTTP_HOST']
+            . '/reset-password?fromPasswordResetEmail=1&id='
+            . $resetPassword->getResetId() . '&key='
+            . $resetPassword->getHashKey();
 
-        foreach ($vars as $name => $value) {
-            $bodyTemplate = str_replace(
-                '__' . $name . '__',
-                $value,
-                $bodyTemplate
-            );
-
-            // Handle BC
-            $bodyTemplate = str_replace(
-                '{' . $name . '}',
-                $value,
-                $bodyTemplate
-            );
-        }
-        try {
-            $html = new MimePart($bodyTemplate);
-            $html->type = "text/html";
-
-            $body = new MimeMessage();
-            $body->setParts([$html]);
-
-            $message = new Message();
-            $message->setBody($body)
-                ->setFrom($fromEmail, $fromName)
-                ->setSubject($subject);
-
-            foreach (explode(',', $toEmail) as $email) {
-                $message->addTo(trim($email));
-            }
-
-            $transport = new \Zend\Mail\Transport\Sendmail();
-
-            $transport->send($message);
-        } catch (InvalidArgumentException $e) {
-            // nothing
-        }
+        $this->templateMailer->send(
+            $toEmail,
+            'donotreply@reliv.com',
+            'Reliv International',
+            'password_reset',
+            [
+                'url' => $resetPasswordLinkUrl
+            ]
+        );
     }
 }
