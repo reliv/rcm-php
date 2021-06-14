2463872e4a6b506459cca3a7cb986c7e77e09134
Rodney Mcnew <rmcnew@Rodneys-MacBook-Pro-2.local>
2021-06-14 14:38:21 -0500

    move password reset process to NodeJS for axo 25680

--- login/config/module.config.php
+++ login/config/module.config.php
@@ -1,27 +1,5 @@
 <?php
 
-/**
- * ZF2 Plugin Config file
- *
- * This file contains all the configuration for the Module as defined by ZF2.
- * See the docs for ZF2 for more information.
- *
- * PHP version 5.3
- *
- * LICENSE: No License yet
- *
- * @category  Reliv
- * @author    Westin Shafer <wshafer@relivinc.com>
- * @copyright 2012 Reliv International
- * @license   License.txt New BSD License
- * @version   GIT: <git_id>
- */
-
-$mergedResetPassword = array_merge(
-    require(__DIR__ . '/createPasswordDefaultInstanceConfig.php'),
-    require(__DIR__ . '/resetPasswordDefaultInstanceConfig.php')
-);
-
 return [
     'asset_manager' => [
         'resolver_configs' => [
@@ -31,8 +9,6 @@ return [
             'collections' => [
                 'modules/rcm-admin/admin.js' => [
                     'modules/rcm-login/rcm-login-edit.js',
-                    'modules/rcm-login/rcm-reset-password-edit.js',
-                    'modules/rcm-login/rcm-create-new-password-edit.js',
                 ],
             ],
         ],
@@ -40,8 +16,6 @@ return [
     'controllers' => [
         'factories' => [
             'RcmLogin' => 'RcmLogin\Factory\PluginControllerFactory',
-            'RcmResetPassword' => 'RcmLogin\Factory\ResetPasswordPluginControllerFactory',
-            'RcmCreateNewPassword' => 'RcmLogin\Factory\CreatePasswordPluginControllerFactory',
         ],
     ],
     'doctrine' => [
@@ -73,29 +47,6 @@ return [
             'redirectBlacklistPattern' => '/.+:\/\/|\/\//i',
             'csrfTimeoutSeconds' => 60 * 60 * 24 * 30 // thirty days worth of seconds
         ],
-        'RcmResetPassword' => [
-            'type' => 'Common',
-            'display' => 'Reset Password',
-            'tooltip' => 'Reset Password',
-            'icon' => '',
-            'defaultInstanceConfig' => $mergedResetPassword,
-            'canCache' => false,
-            'mailer' => 'RcmLogin\Email\DefaultMailer',
-        ],
-        /**
-         * @deprecated - The RcmResetPassword plugin now handles both the first and second
-         * page of the password reset process. The 2nd page will be rendered if
-         * fromPasswordResetEmail=1 is in the url. This one done to reduce the number of pages
-         * needed for this process to reduce brittleness.
-         */
-        'RcmCreateNewPassword' => [
-            'type' => 'Common',
-            'display' => 'Create New Password',
-            'tooltip' => 'Create New Password',
-            'icon' => '',
-            'defaultInstanceConfig' => $mergedResetPassword,
-            'canCache' => false,
-        ],
     ],
     'service_manager' => [
         'factories' => [
@@ -110,12 +61,6 @@ return [
 
             'RcmLogin\Email\DefaultMailer'
             => 'RcmLogin\Factory\DefaultMailerFactory',
-
-            \RcmLogin\InputFilter\CreateNewPasswordInputFilter::class
-            => \RcmLogin\Factory\CreateNewPasswordInputFilterFactory::class,
-
-            \RcmLogin\InputFilter\ResetPasswordInputFilter::class
-            => \RcmLogin\Factory\ResetPasswordInputFilterFactory::class,
 
             /* over-ride this for logging */
             \RcmLogin\Log\Logger::class
